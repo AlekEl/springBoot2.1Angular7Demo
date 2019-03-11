2f645137e538a21e6c149e11d9af9d12c940979b
AlekEl <leszczynski.al@gmail.com>
2019-03-11 13:44:56 +0100

    Add question controller

---
+++ src/main/java/com/codecool/demo/Controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.codecool.demo.Controller;
+
+import com.codecool.demo.Model.Question;
+import com.codecool.demo.Model.QuestionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RestController
+public class QuestionController {
+
+    private QuestionRepository repository;
+
+    public QuestionController(QuestionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/questions")
+    public Collection<Question> questions() {
+        return new ArrayList<>(repository.findAll());
+    }
+}
