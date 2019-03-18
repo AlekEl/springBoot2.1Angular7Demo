c97572b768772daeee1a9465fdd73cb0d47626fe
AlekEl <leszczynski.al@gmail.com>
2019-03-18 11:23:16 +0100

    Add navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionListComponent } from './question-list/question-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionListComponent
+    QuestionListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
